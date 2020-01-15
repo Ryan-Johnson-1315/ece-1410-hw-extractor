33fe6e85b34b36dc30cb5a979ecee766a03ae3d6
Ryan Johnson <Ryan.Johnson.1315@gmail.com>
2020-01-14 23:06:00 -0700

    Added argparse

--- src/extract.py
+++ src/extract.py
@@ -2,14 +2,26 @@ import os
 from sys import argv
 import zipfile
 from shutil import copyfile, move
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser('Program that extracts all files in zip folders and compiles them')
+
+parser.add_argument('--submissions', help='Path the submissions zip folder', required=True)
+parser.add_argument('--solutions', help='Path to test.cpp file', required=True)
+parser.add_argument('--compile', help='Whether to compile the code while extracting', action='store_true')
+parser.add_argument('--folder', help='Name of the folder to create (i.e hw3)', required=True)
+
+args = parser.parse_args()
+
 
 gtest_files = os.listdir(os.curdir + '/gtest')
 gtest_path = 'gtest/'
 
-solution_file = argv[3]
+solution_file = args.solutions
 solution_path = os.curdir + '/'
 
-homework_dir = argv[1] + ('/' if not argv[1].endswith('/') else '')
+homework_dir = args.folder + ('/' if not args.folder.endswith('/') else '')
 submission_dir = homework_dir + 'submissions/'
 
 # Make all the dirs
@@ -30,37 +42,49 @@ def copy_gstest(dest):
         copyfile(gtest_path + i, dest + i)
 
 def unzip_submissions():
-    move(argv[2], submission_dir)
-    with zipfile.ZipFile(submission_dir + argv[2]) as zip:
+    move(args.submissions, submission_dir)
+    with zipfile.ZipFile(submission_dir + args.submissions) as zip:
         zip.extractall(submission_dir)
-    os.remove(submission_dir + argv[2])
+    os.remove(submission_dir + args.submissions)
 
 
 unzip_submissions()
 
 count = 0
+not_compiled = list()
 for i in os.listdir(submission_dir):
     if ".zip" in i:
-        print(f"Extracting: {submission_dir + i}")
+        print(f"\nExtracting: {submission_dir + i}")
 
         folder_name = submission_dir + i.split('.zip')[0] + '/'
         zip_file = submission_dir + i
 
         if not os.path.isdir(folder_name):
-            print(f'Creating: {folder_name}')
+            print(f'\tCreating: {folder_name}')
             os.mkdir(folder_name)
 
         with zipfile.ZipFile(zip_file, 'r') as zip:
-            print(f'Extracting files to: {folder_name}')
+            print(f'\tExtracting files to: {folder_name}')
             zip.extractall(folder_name)
         
         copy_gstest(folder_name)
         copy_solution(folder_name)
         
         open(folder_name + 'grade.txt', 'w')
-        print('Success!')
+        print(f'Extracted {len(os.listdir(folder_name))} files')
         os.remove(zip_file)
+
+        if args.compile:
+            print('Compiling Code...')
+
+            subprocess.call(f'g++ *.cpp', shell=True, cwd=folder_name)  
+            if not os.path.exists(f'{folder_name}a.out'):
+                not_compiled.append(folder_name)
+            
         count += 1
 
+if args.compile:
+    print(f'Compiled {count} directories')
+    print('\n\t'.join(not_compiled) + 'Not Compiled')
 print(f'\nExtracted {count} directories')
 
