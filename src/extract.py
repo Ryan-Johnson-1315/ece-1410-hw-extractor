3e4fd83010563e597d4c006f59eff779623a5188
Ryan Johnson <Ryan.Johnson.1315@gmail.com>
2020-01-17 11:01:15 -0700

    Should go through any directories now

--- src/extract.py
+++ src/extract.py
@@ -47,7 +47,22 @@ def unzip_submissions():
         zip.extractall(submission_dir)
     os.remove(submission_dir + args.submissions)
 
-
+def extract_sub_dirs(directory, destination):
+    for file in os.listdir(directory):
+        print(f'Checking: {directory + file}...', end='')
+        #print(f'if os.path.isfile({file})[{os.path.isfile(file)}] and (".cpp" in {file} or ".hpp" in {file} or ".h" in {file}[{(".cpp" in file or ".hpp" in file or ".h" in file)}])')
+        if os.path.isfile(directory + file) and (".cpp" in file or ".hpp" in file or ".h" in file) and "gtest" not in file:
+            print(f'is a source file')
+            if directory != destination:
+                print(f'Copying to {destination}')
+                copyfile(directory + file, destination + file)
+        elif os.path.isdir(directory + file + '/'):
+            print('is a directory')
+            input(f'Found sub dir: {file}')
+            extract_sub_dirs(directory + file + '/', destination)
+        else:
+            print('Neither')
+        input('')
 unzip_submissions()
 
 count = 0
@@ -67,6 +82,8 @@ for i in os.listdir(submission_dir):
             print(f'\tExtracting files to: {folder_name}')
             zip.extractall(folder_name)
         
+        extract_sub_dirs(folder_name, folder_name)
+
         copy_gstest(folder_name)
         copy_solution(folder_name)
         
